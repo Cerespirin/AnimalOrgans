891fb292df8d5daa315a71e8e63fc4e0ec6b5335
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2023-12-21 04:42:20 -0500

    Initial commit

---
+++ Source/LuluAnimalOrgans/CompOrganOrigin.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.AnimalOrgans
+{
+	public class CompOrganOrigin : ThingComp
+	{
+		// Default to human originator.
+		public ThingDef originDef = ThingDefOf.Human;
+
+		public override void PostExposeData()
+		{
+			Scribe_Defs.Look(ref originDef, nameof(originDef));
+		}
+
+		public override void PostPostGeneratedForTrader(TraderKindDef trader, int forTile, Faction forFaction)
+		{
+			// 20% chance generated organ will be for an animal when generated for a trader.
+			if (Rand.Chance(0.2f))
+			{
+				// Select a random pawn def that this trader will trade.
+				if (DefDatabase<ThingDef>.AllDefs.Where(d => typeof(Pawn).IsAssignableFrom(d.thingClass) && trader.WillTrade(d)).TryRandomElement(out ThingDef result))
+				{
+					// Set the organ's origin to that def.
+					originDef = result;
+				}
+			}
+		}
+
+		public override string TransformLabel(string label)
+		{
+			// Will change "lung" to "human lung", for example.
+			return originDef.label + " " + label;
+		}
+
+		public override bool AllowStackWith(Thing other)
+		{
+			CompOrganOrigin otherComp = other.TryGetComp<CompOrganOrigin>();
+			return (otherComp != null) && (otherComp.originDef == originDef);
+		}
+
+		public override void PostSplitOff(Thing piece)
+		{
+			base.PostSplitOff(piece);
+			piece.TryGetComp<CompOrganOrigin>().originDef = originDef;
+		}
+	}
+}
---
+++ Source/LuluAnimalOrgans/MyStaticConstructor.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Text;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.AnimalOrgans
+{
+	[StaticConstructorOnStartup]
+	public static class MyStaticConstructor
+	{
+		static MyStaticConstructor()
+		{
+			// Execute our Harmony patches.
+			Harmony harmony = new Harmony("rimworld.loonyladle.animalorgans");
+			harmony.PatchAll();
+
+			StringBuilder stringBuilder = new StringBuilder("[LuluAnimalOrgans] Dynamic patched the following defs: ");
+			bool first = true;
+
+			// Search all ThingDefs in the DefDatabase.
+			foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefsListForReading)
+			{
+				if ((thingDef.description?.Contains(ThingDefOf.Human.label) ?? false) && (thingDef.thingCategories?.Contains(MyDefOf.BodyPartsNatural) ?? false))
+				{
+					// Remove all instances of "human" in the def's label, then replace unwanted double spaces this might have caused.
+					thingDef.description = thingDef.description.Replace(ThingDefOf.Human.label, null).Replace("  ", " ");
+
+					if (first)
+					{
+						stringBuilder.Append(thingDef.defName);
+						first = false;
+					}
+					else
+					{
+						stringBuilder.AppendWithComma(thingDef.defName);
+					}
+				}
+			}
+			// Report on patched defs.
+			Log.Message(stringBuilder.ToString());
+		}
+	}
+}
