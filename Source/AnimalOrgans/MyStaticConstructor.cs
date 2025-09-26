d787c8afd3084fd38a340a5a25e6c4b0582e1d12
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-26 14:50:07 -0400

    Remove patch logging
    No player needs or cares to know about this. Keep the log clean.

--- Source/AnimalOrgans/MyStaticConstructor.cs
+++ Source/AnimalOrgans/MyStaticConstructor.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using RimWorld;
-using System.Text;
 using Verse;
 
 namespace Cerespirin.AnimalOrgans
@@ -13,28 +12,14 @@ namespace Cerespirin.AnimalOrgans
 			Harmony harmony = new Harmony("rimworld.cerespirin.animalorgans");
 			harmony.PatchAll();
 
-			StringBuilder stringBuilder = new StringBuilder("[AnimalOrgans] Dynamic patched the following defs: ");
-			bool first = true;
-
 			foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefsListForReading)
 			{
 				if ((thingDef.description?.Contains(ThingDefOf.Human.label) ?? false) && (thingDef.thingCategories?.Contains(MyDefOf.BodyPartsNatural) ?? false))
 				{
 					// Remove all instances of "human" in the def's label, then replace unwanted double spaces this might have caused.
 					thingDef.description = thingDef.description.Replace(ThingDefOf.Human.label, null).Replace("  ", " ");
-
-					if (first)
-					{
-						stringBuilder.Append(thingDef.defName);
-						first = false;
-					}
-					else
-					{
-						stringBuilder.AppendWithComma(thingDef.defName);
-					}
 				}
 			}
-			Log.Message(stringBuilder.ToString());
 		}
 	}
 }
